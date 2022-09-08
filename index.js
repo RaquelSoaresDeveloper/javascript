4512766d32f00b3a6f35668cc2081d68606a24b0
RaquelSoaresDeveloper <rp19991103@gmail.com>
2022-09-08 10:00:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let nome = "joana";
+//alert (nome)
+//console.log(nome)
+
+nome = prompt("escreva seu nome");
+idade = prompt("escreva sua idade");
+gostomusical = prompt("escreva seu gosto musical");
+filmefavorito = prompt("escreva seu filme favorito");
+filho = ("escreva quantos filhos você tem");
+sonho =("escreva seu sonho")
+alert ("seu nome é " + nome + "sua idade é " + idade + "seu gosto musical é " + 
+gostomusical +"seu filme favorito é " + " não  " + filho + "seu sonho é " + sonho);
+
